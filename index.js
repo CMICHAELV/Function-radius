b862d795b20853f690f78908344915d58c916526
CMICHAELV <mikec4dmasses@gmail.com>
2023-08-01 01:28:05 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function calculateAreaOfcircle (radius){
+    const pi = 3.14
+    const area = pi * (2 * radius)
+    return area
+}
+const radius = 8
+const circleArea = calculateAreaOfcircle(radius)
+console.log("the area of the circle is " + circleArea);
